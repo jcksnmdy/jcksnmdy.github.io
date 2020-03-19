e0e096f545936757c5a114f9519b32cd507cb91a
Jackson Moody <jcksnmdy1@gmail.com>
2020-03-19 01:32:00 -0400

    added points

--- asset.js
+++ asset.js
@@ -11,7 +11,7 @@ function allowDrop(ev) {
         var data = ev.dataTransfer.getData("text"); 
         ev.target.appendChild(document.getElementById(data)); 
 } 
-function pointUp(ev) {
+function pointUp() {
     var score = document.getElementById('points');
     var number = score.innerHTML;
     number++;
